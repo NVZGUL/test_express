79fdbe6b6ffa83836e8989143d69d9da20076960
NVZGUL <h3rmess@gmail.com>
2017-06-28 22:18:23 +0300

    ff

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express()
+var port = process.env.PORT || 3000
 
 app.get('/', function(req,res){
 	res.send('Hello world32')
 });
 
-var server = app.listen(3000, function(req,res){
-	console.log("server runnin on http://localhost:"+server.address().port);
+var server = app.listen(port, function(req,res){
+	console.log("server runnin on http://localhost:"+port);
 })
---
+++ tests/test.js
@@ -0,0 +1,6 @@
+var foo = 'bar'
+describe('Truth', function(){
+	it('should equal bar', function(){
+		foo.should.equal('bar')
+	})
+})
